ebd8c87b83a15faae0e445bd15615455481fa52c
Angga Arya Saputra <anggaarya53@gmail.com>
2019-01-13 20:23:34 +0700

    Initial commit

---
+++ app/src/main/java/indonesia/angasalabs/teleponexample/MainActivity.java
@@ -0,0 +1,66 @@
+package indonesia.angasalabs.teleponexample;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity  {
+    private  Button call;
+    private EditText editText;
+
+    private String numberTelp = "";
+
+    private static final int PHONE_REQUEST_CODE = 986;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+
+        editText = findViewById(R.id.et_main);
+
+        call = findViewById(R.id.btn_call);
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calling();
+            }
+        });
+
+
+    }
+
+    private void calling(){
+        numberTelp = editText.getText().toString();
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE)
+                == PackageManager.PERMISSION_GRANTED) {
+            Intent intent = new Intent(Intent.ACTION_CALL);
+            intent.setData(Uri.parse("tel:" + numberTelp));
+            startActivity(intent);
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CALL_PHONE}, PHONE_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == PHONE_REQUEST_CODE){
+            calling();
+        } else {
+            Toast.makeText(getApplicationContext(), "Aplikasi tidak diizinkan", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
